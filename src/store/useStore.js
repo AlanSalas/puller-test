bff6ad677820722847424687e7e60c314b88a58b
Alan Salas <46615148+AlanSalas@users.noreply.github.com>
2022-08-07 14:58:34 -0500

    Add zustand devtools

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,4 +1,5 @@
 import create from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 import authSlice from "store/slices/authSlice";
 import productSlice from "store/slices/productSlice";
 import uiSlice from "store/slices/uiSlice";
@@ -8,5 +9,7 @@ const useStore = create((set, get) => ({
   ...productSlice(set, get),
   ...uiSlice(set, get),
 }));
+
+mountStoreDevtool("Store", useStore);
 
 export default useStore;
