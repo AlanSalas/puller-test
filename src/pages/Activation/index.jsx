d0a49c7ea7a89d19b9fb26cb94c7cece36f9b87a
Alan Salas <46615148+AlanSalas@users.noreply.github.com>
2022-08-08 06:40:07 -0500

    Add activation page

--- src/pages/Activation/index.jsx
+++ src/pages/Activation/index.jsx
@@ -1,7 +1,19 @@
+import Card from "components/Card";
+import Button from "components/Button";
+import "./style.css";
+
 const Activation = () => {
   return (
-    <div>
-      <h1>Activation</h1>
+    <div className="activation">
+      <div className="container">
+        <h1 className="color-tertiary text-center">Acivate your account</h1>
+        <Card style={{ width: "100%", padding: "5rem 0.8rem" }}>
+          <h3 className="color-tertiary text-center">Activate your account to login in the App!</h3>
+          <div className="activation-action">
+            <Button type="btn--success">Activate</Button>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 };
